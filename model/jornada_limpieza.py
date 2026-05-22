61440e762647a86edd4da12b4c102f65d4f67498
Angelica Garro <angegarrom17@gmail.com>
2026-05-22 09:46:35 -0600

    primer commit

---
+++ model/jornada_limpieza.py
@@ -0,0 +1,26 @@
+class JornadaLimpieza:
+    def __init__(self, idVoluntario: str, nombre: str, telefono:str, edad:int, correo:str, organizacion:str):
+        self.idVoluntario = idVoluntario
+        self.nombre = nombre
+        self.telefono = telefono
+        self.edad = edad
+        self.correo = correo
+        self.organizacion = organizacion
+
+
+    def to_dict(self) -> dict:
+        return {
+            "idVoluntario": self.idVoluntario,
+            "nombre": self.nombre,
+            "telefono": self.telefono,
+            "edad": self.edad,
+            "correo":self.correo,
+            "organizacion":self.organizacion
+        }
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data["idVoluntario"], data["nombre"], data["telefono"], data["edad"], data["correo"], data["organizacion"])
+
+    def __str__(self) -> str:
+        return f"ID: {self.idVoluntario} - Nombre: {self.nombre} - Telefono: {self.telefono} - Edad: {self.edad} - Correo: {self.correo} - Organizacion: {self.organizacion}"
---
+++ repository/voluntario_repository.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+class VoluntarioRepositorio:
+    def __init__(self, filename="voluntarios.json"):
+        self.filename = filename
+        self._voluntarios = []
+
+        self._load()
+
+#--------------------------------------------------------------------------------
+    def _load(self):
+        if not os.path.exists(self.filename):
+            self._voluntarios = []
+            return
+
+        with open(self.filename, "r", encoding="utf-8") as f:
+            self._voluntarios = json.load(f)
+
+#---------------------------------------------------------------------------------
+    def _save(self):
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump(self._voluntarios, f, indent=4)
+
+#----------------------------------------------------------------------------
+    def obtener_todos(self):
+        self._load()
+        return self._voluntarios
+
+#---------------------------------------------------------------------------
+    def agregar(self, voluntario):
+        self._voluntarios.append(voluntario.to_dict())
+        self._save()
