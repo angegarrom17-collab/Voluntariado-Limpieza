bc59a698b050b7d090bf86c41bf883561b1af5c2
Angelica Garro <angegarrom17@gmail.com>
2026-05-24 22:10:24 -0600

    primer commit

--- model/jornada_limpieza.py
+++ model/jornada_limpieza.py
@@ -1,26 +1,36 @@
 class JornadaLimpieza:
-    def __init__(self, idVoluntario: str, nombre: str, telefono:str, edad:int, correo:str, organizacion:str):
-        self.idVoluntario = idVoluntario
-        self.nombre = nombre
-        self.telefono = telefono
-        self.edad = edad
-        self.correo = correo
-        self.organizacion = organizacion
+
+    def __init__(self, idJornada: str, fecha: str,descripcion: str, cantidadBasuraTotal: int,observaciones: str):
+        self.idJornada = idJornada
+        self.fecha = fecha
+        self.descripcion = descripcion
+        self.cantidadBasuraTotal = cantidadBasuraTotal
+        self.observaciones = observaciones
 
 
     def to_dict(self) -> dict:
         return {
-            "idVoluntario": self.idVoluntario,
-            "nombre": self.nombre,
-            "telefono": self.telefono,
-            "edad": self.edad,
-            "correo":self.correo,
-            "organizacion":self.organizacion
+            "idJornada": self.idJornada,
+            "fecha": self.fecha,
+            "descripcion": self.descripcion,
+            "cantidadBasuraTotal": self.cantidadBasuraTotal,
+            "observaciones": self.observaciones
         }
 
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(data["idVoluntario"], data["nombre"], data["telefono"], data["edad"], data["correo"], data["organizacion"])
+        return cls(data["idJornada"],
+            data["fecha"],
+            data["descripcion"],
+            data["cantidadBasuraTotal"],
+            data["observaciones"]
+        )
 
     def __str__(self) -> str:
-        return f"ID: {self.idVoluntario} - Nombre: {self.nombre} - Telefono: {self.telefono} - Edad: {self.edad} - Correo: {self.correo} - Organizacion: {self.organizacion}"
+        return (
+            f"ID Jornada: {self.idJornada} - "
+            f"Fecha: {self.fecha} - "
+            f"Descripcion: {self.descripcion} - "
+            f"Cantidad Basura Total: {self.cantidadBasuraTotal} kg - "
+            f"Observaciones: {self.observaciones}"
+        )
